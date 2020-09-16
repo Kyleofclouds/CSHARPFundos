9fd53418232da414af80b37539dc09ba7634c5e9
Kyleoftheclouds <kylewhitmore@outlook.com>
2020-09-15 20:21:52 -0400

    Day Four

---
+++ 06_Inheritance_Classes/Cat.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_Inheritance_Classes
+{
+    public class Cat : Animal
+    {
+        public double ClawLength { get; set; }
+        public Cat()
+        {
+            Console.WriteLine("This is the cat constructor");
+            IsMammel = true;
+            TypeOfDiet = DietType.Carnivore;
+        }
+        public override void Move()
+        {
+            Console.WriteLine($"The {GetType().Name} moves quickly.");
+        }
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Meow.");
+        }
+    }
+    public class Liger : Cat
+    {
+        public Liger()
+        {
+            Console.WriteLine("This is the Liger constructor");
+        }
+        public override void Move()
+        {
+            Console.WriteLine($"THe {GetType().Name} stalks its prey");
+            base.Move();
+        }
+        public override void MakeSound()
+        {
+            Console.WriteLine("Roar");
+        }
+    }
+}
---
+++ 06_Inheritance_Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("06_Inheritance_Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("06_Inheritance_Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("28206e5d-b5a1-4d6b-ac55-b3579596c55d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
